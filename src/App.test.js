77aacf7ac097af07af879d4122cd1b3d2c738e73
Nazare Emanuel-Ioan <emanuelioannazare@gmail.com>
2021-07-08 15:07:38 +0300

    feature: add mock test

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,18 @@
-import { add, total } from "./App";
+// import { total } from "./App";
+
+const add = jest.fn(() => 3);
 
 // Unit test
 // It only tests one thing
 test("add", () => {
-  // const value = add(1, 2);
-  // expect(value).toBe(3);
   expect(add(1, 2)).toBe(3);
-  expect(add(5, 2)).toBe(7);
+  expect(add).toHaveBeenCalledTimes(1);
+  expect(add).toHaveBeenCalledWith(1, 2);
+  // expect(add(5, 2)).toBe(7);
 });
 
-test("total", () => {
-  expect(total(5, 20)).toBe("$25");
-});
+// Integration tests
+// Tests things working toghether
+// test("total", () => {
+//   expect(total(5, 20)).toBe("$25");
+// });
